abff92d2b40bef627eb222a083beaef0195d7022
Vladimir Vorozhtcov <lastlilith.2014@gmail.com>
2023-07-14 16:17:21 +0900

    Added baic splash screen and first auth screen

--- app/src/main/java/com/example/testingchat/SplashActivity.kt
+++ app/src/main/java/com/example/testingchat/SplashActivity.kt
@@ -1,11 +1,29 @@
 package com.example.testingchat
 
+import android.annotation.SuppressLint
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.util.Timer
+import java.util.TimerTask
 
+@SuppressLint("CustomSplashScreen")
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Timer().schedule(object : TimerTask() {
+            override fun run() {
+                val intent = Intent(this@SplashActivity, AuthActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }, DELAY)
+
+    }
+
+    companion object {
+        private const val DELAY: Long = 1000
     }
 }
